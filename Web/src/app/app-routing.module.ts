088f3ca343a86812fe5841f75acd2861bb6023f2
Mahiransh Sharma <51320070+mahisharma-cs@users.noreply.github.com>
2020-12-17 17:43:23 +0530

    Add files via upload

---
+++ Web/src/app/app-routing.module.ts
@@ -0,0 +1,78 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { HomeComponent} from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import {FileExplorerComponent} from './components/file-explorer/file-explorer.component';
+import { BoardComponent } from './components/board/board.component';
+import { AssignmentComponent } from './components/assignment/assignment.component';
+
+// guard
+import { AuthGuard } from './shared/auth.guard';
+import { NewBoardComponent } from './components/new-board/new-board.component';
+import { ContributionComponent } from './components/contribution/contribution.component';
+import { FilesComponent } from './components/files/files.component';
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'sign-up',
+    component: SignUpComponent
+  },
+  {
+    path: 'contribution',
+    component: ContributionComponent
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'file-explorer',
+    component: FileExplorerComponent,
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'board',
+    component: BoardComponent,
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'folder/:id',
+    component: FilesComponent,
+  },
+  {
+    path: 'assignment',
+    component: AssignmentComponent,
+    // canActivate: [AuthGuard]
+  },
+  {
+    path: 'creative-board',
+    component: NewBoardComponent,
+    canActivate: [AuthGuard]
+  },
+  {
+    path: '**',
+    redirectTo: 'home'
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
